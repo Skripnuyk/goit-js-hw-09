22c79c2962afc33ab0d7083773274c02126dd2b1
Skripnuyk <walker8613@gmail.com>
2023-03-02 17:52:21 +0200

    added task 3 solution

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,17 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const inputDate = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const timerDiv = document.querySelector('.timer');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+
+startBtn.disabled = true;
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -17,6 +31,45 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    };
+  },
+};
+
+flatpickr(inputDate, options);
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  let timer = setInterval(() => {
+    let countdown = new Date(inputDate.value) - new Date();
+    startBtn.disabled = true;
+
+    if (countdown >= 0) {
+      let timeDate = convertMs(countdown);
+      days.textContent = addLeadingZero(timeDate.days);
+      hours.textContent = addLeadingZero(timeDate.hours);
+      minutes.textContent = addLeadingZero(timeDate.minutes);
+      seconds.textContent = addLeadingZero(timeDate.seconds);
+      if (countdown <= 60000) {
+        timerDiv.style.color = 'tomato';
+      }
+    } else {
+      Notiflix.Notify.success('Countdown finished');
+      timerDiv.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+});
